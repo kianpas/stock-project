5dc267e378ba0b272635afb663df7602096a2c12
kianpas <kianpas@gmail.com>
2025-03-06 13:59:20 +0900

    결과 소수점 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,13 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {stockData.longName} : {stockData.strDev}
-        <p> {stockData.stdDevPercent}</p>
+        {stockData.longName}
+        <p>표준편차 {stockData.stdDevPercent}%</p>
         <p>거래량 {stockData.sample.meta.regularMarketVolume}</p>
+        <p>일일변동량 {stockData.dailyVarRate}%</p>
+        <p>현재가격 대비 52주 최고 대비 하락 {stockData.ftLower}%</p>
+        <p>현재가격 대비 52주 최저 대비 상승 {stockData.ftHigher}%</p>
+        <p>일일변동폭 {stockData.dailyVarLength}%</p>
       </main>
 
     </div>
